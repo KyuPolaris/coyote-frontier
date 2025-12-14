2459f768e04d9494101f4c92a2fb71ed54df7d7b
Danny Kay <sprambersonson@gmail.com>
2025-12-13 21:10:52 -0800

    oh the rest of it

--- Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
+++ Content.Client/Lobby/UI/HumanoidProfileEditor.xaml.cs
@@ -625,7 +625,7 @@ namespace Content.Client.Lobby.UI
             }
 
             // Categories that should be shown in a nested window instead of inline
-            var nestedCategories = new HashSet<string> { "Temperament", "SpeechTraits", "Disabilities" };
+            var nestedCategories = new HashSet<string> { "Temperament", "SpeechTraits", "Disabilities", "BodyType" };
 
             // Create UI view from model
             foreach (var (categoryId, categoryTraits) in traitGroups)
